256c7453461f1d3c3bbdc5467387d1f3627accae
xiaozhu <1351095072@qq.com>
2019-12-07 09:59:39 +0800

    更新水牛城滞销 帮帮我们

--- benzhu/spiders/itcast.py
+++ benzhu/spiders/itcast.py
@@ -18,37 +18,40 @@ class ItcastSpider(scrapy.Spider):
 
     #解析的方法，每个初始URL完成下载后将被调用
     def parse(self, response):
-        #获取上一次number的数据
-        jsonFile = JsonFile()
-        oldnumber = jsonFile.getJsonFile();
-        print("上一次查询的价格为："+str(oldnumber))
-        #把表更新为最新的数据
-        jsonFile.setJsonFile(response)
-        #解析json数据
-        jsobj = json.loads(response.body)
-        #获取数据模型
-        item = VirmachItem()
-        #放入数据
-        price = str(jsobj['price'])
-        newprice = price[1:price.index(' ')]
-        print("本次查询的价格为："+newprice)
-        item['price'] = newprice
-        item['virt'] = str(jsobj['virt'])
-        item['ram'] = str(jsobj['ram'])
-        item['cpu'] = str(jsobj['cpu'])
-        item['hdd'] = str(jsobj['hdd'])
-        item['bw'] = str(jsobj['bw'])
-        item['ips'] = str(jsobj['ips'])
-        addres = str(jsobj['location'])
-        item['addres'] = addres
-        #获取价格小数点之前的位数 判断价格是否小于等于15元
-        number = price[1:price.index(".")]
-        JsonFile.textLog(newprice,oldnumber,item)
-        if(int(number) <=15 and oldnumber!=newprice):
-            #小于15元 发送邮件
-            benzhuEmail = BenzhuEmail()
-            benzhuEmail.send(item)
-            if (addres == 'CHICAGO, IL' or addres == 'PISCATAWAY, NJ' or addres =='SAN JOSE, CA'):
-                #等于这三个地区的话就发送短信
-                message = Message()
-                #message.sendMessage(newprice)
+        if(response.text == 'None' or response.text == ""):
+            print("获取内容失败")
+        else:
+            #获取上一次number的数据
+            jsonFile = JsonFile()
+            oldnumber = jsonFile.getJsonFile();
+            print("上一次查询的价格为："+str(oldnumber))
+            #把表更新为最新的数据
+            jsonFile.setJsonFile(response)
+            #解析json数据
+            jsobj = json.loads(response.body)
+            #获取数据模型
+            item = VirmachItem()
+            #放入数据
+            price = str(jsobj['price'])
+            newprice = price[1:price.index(' ')]
+            print("本次查询的价格为："+newprice)
+            item['price'] = newprice
+            item['virt'] = str(jsobj['virt'])
+            item['ram'] = str(jsobj['ram'])
+            item['cpu'] = str(jsobj['cpu'])
+            item['hdd'] = str(jsobj['hdd'])
+            item['bw'] = str(jsobj['bw'])
+            item['ips'] = str(jsobj['ips'])
+            addres = str(jsobj['location'])
+            item['addres'] = addres
+            #获取价格小数点之前的位数 判断价格是否小于等于15元
+            number = price[1:price.index(".")]
+            JsonFile.textLog(newprice,oldnumber,item)
+            if(int(number) <=15 and oldnumber!=newprice):
+                #小于15元 发送邮件
+                benzhuEmail = BenzhuEmail()
+                benzhuEmail.send(item)
+                if (addres == 'CHICAGO, IL' or addres == 'PISCATAWAY, NJ' or addres =='SAN JOSE, CA' or addres == 'San Jose, CA'):
+                    #等于这三个地区的话就发送短信
+                    message = Message()
+                    #message.sendMessage(newprice)
